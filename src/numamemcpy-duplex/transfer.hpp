e7254d22129154f4942b5072be40f09452077fd4
Carl Pearson <carl.pearson@us.ibm.comwq>
2018-09-23 08:13:41 -0400

    rework numamemcpy-duplex/host

--- src/numamemcpy-duplex/transfer.hpp
+++ src/numamemcpy-duplex/transfer.hpp
@@ -6,7 +6,7 @@
 #define OR_RETURN(x) \
 { \
     cudaError_t err = (x); \
-    if (cudaSuccess != x) { \
+    if (cudaSuccess != err) { \
         return err; \
     } \
 }
@@ -14,45 +14,47 @@
 class CudaMemcpyConfig {
   public:
   cudaMemcpyKind kind_;
-  int src_id_ // cuda device ID or numa ID
+  int src_id_; // cuda device ID or numa ID
   int dst_id_;
   void *src_; // pointer to source allocation
   void *dst_;
-  size_t bytes_; // size of transfer / allocation
   cudaStream_t stream_; // stream for the transfer
   cudaEvent_t start_; // start time of the transfer
   cudaEvent_t stop_; // stop time of transfer
 
   CudaMemcpyConfig() : kind_(cudaMemcpyDefault) {}
-  CudaMemcpyConfig(const cudaMemcpyKind kind, const size_t bytes, const int src, const int dst)
-   : kind_(kind), src_id_(src), dst_id_(dst), bytes_(bytes) {}
+  CudaMemcpyConfig(const cudaMemcpyKind kind, const int src, const int dst)
+   : kind_(kind), src_id_(src), dst_id_(dst) {}
 
-  virtual cudaError_t gpu_allocate() = 0;
-  virtual cudaError_t gpu_free() = 0;
+  virtual cudaError_t cpu_allocate(const size_t bytes) = 0;
+  virtual cudaError_t cpu_free() = 0;
 
-  cudaError_t init() {
-      cpu_allocate();
+  cudaError_t init(const size_t bytes) {
+
+      OR_RETURN(cpu_allocate(bytes));
 
       if (cudaMemcpyHostToDevice == kind_) {
           OR_RETURN(cudaMalloc(&dst_, bytes));
-      } else if { cudaMemcpyDeviceToHost == kind_} {
+          OR_RETURN(cudaMemset(dst_, 0, bytes));
+      } else if (cudaMemcpyDeviceToHost == kind_) {
           OR_RETURN(cudaMalloc(&src_, bytes));
+          OR_RETURN(cudaMemset(src_, 0, bytes));
       } else {
           assert(0 && "Unexpected kind_");
       }
 
       OR_RETURN(cudaStreamCreate(&stream_));
       OR_RETURN(cudaEventCreate(&start_));
-      OR_RETURN(cudaEventCreate(&stop_));
-  }
+      return cudaEventCreate(&stop_);
+}
 
   cudaError_t fini() {
-      cpu_free();
+      OR_RETURN(cpu_free());
 
       if (cudaMemcpyHostToDevice == kind_) {
-          OR_RETURN(cudaFree(dst_);
-      } else if { cudaMemcpyDeviceToHost == kind_} {
-          OR_RETURN(cudaMalloc(src_);
+          OR_RETURN(cudaFree(dst_));
+      } else if (cudaMemcpyDeviceToHost == kind_) {
+          OR_RETURN(cudaFree(src_));
       } else {
           assert(0 && "Unexpected kind_");
       }
@@ -60,62 +62,94 @@ class CudaMemcpyConfig {
       OR_RETURN(cudaStreamDestroy(stream_));
       OR_RETURN(cudaEventDestroy(start_));
       OR_RETURN(cudaEventDestroy(stop_));
+      return cudaSuccess;
+  }
+
+  cudaError_t reset_gpu() {
+      if (cudaMemcpyHostToDevice == kind_) {
+          return utils::cuda_reset_device(dst_id_);
+      } else if (cudaMemcpyDeviceToHost == kind_) {
+          return utils::cuda_reset_device(src_id_);
+      } else {
+          assert(0 && "Unexpected kind_");
+      }
+      return cudaSuccess;  
   }
 };
 
 
 
 class PageableCopyConfig : public CudaMemcpyConfig {
-    PageableCopy(const cudaMemcpyKind kind, const size_t bytes, const int src, const int dst)
-     : CudaMemcpyConfig(kind, bytes, src, dst) {}
+  public:
+    PageableCopyConfig(const cudaMemcpyKind kind, const int src, const int dst)
+     : CudaMemcpyConfig(kind, src, dst) {}
 
-    cudaError_t cpu_allocate() {
+    cudaError_t cpu_allocate(const size_t bytes) {
+
         if (cudaMemcpyHostToDevice == kind_) {
-            dst_ = aligned_alloc(page_size(), bytes);
-        } else if { cudaMemcpyDeviceToHost == kind_} {
+            numa_bind_node(src_id_);
             src_ = aligned_alloc(page_size(), bytes);
+            std::memset(src_, 0, bytes);
+        } else if (cudaMemcpyDeviceToHost == kind_) {
+            numa_bind_node(dst_id_);
+            dst_ = aligned_alloc(page_size(), bytes);
+            std::memset(dst_, 0, bytes);
         } else {
             assert(0 && "Unexpected kind_");
         }
+        numa_bind_node(-1);
+        return cudaSuccess;
     }
 
     cudaError_t cpu_free() {
         if (cudaMemcpyHostToDevice == kind_) {
-            free(dst_);
-        } else if { cudaMemcpyDeviceToHost == kind_} {
             free(src_);
+        } else if (cudaMemcpyDeviceToHost == kind_) {
+            free(dst_);
         } else {
             assert(0 && "Unexpected kind_");
-        }        
+        }
+        return cudaSuccess;
     }
-}
+};
 
 
 class PinnedCopyConfig : public CudaMemcpyConfig {
-    PinnedCopy(const cudaMemcpyKind kind, const size_t bytes, const int src, const int dst)
-     : CudaMemcpyConfig(kind, bytes, src, dst) {}
+  public:
+    PinnedCopyConfig(const cudaMemcpyKind kind, const int src, const int dst)
+     : CudaMemcpyConfig(kind, src, dst) {}
 
-    cudaError_t  cpu_allocate() {
+    cudaError_t  cpu_allocate(const size_t bytes) {
         if (cudaMemcpyHostToDevice == kind_) {
-            dst_ = aligned_alloc(page_size(), bytes_);
-            OR_RETURN(cudaHostRegister(dst_, bytes, 0));
-        } else if { cudaMemcpyDeviceToHost == kind_} {
-            src_ = aligned_alloc(page_size(), bytes_);
-            OR_RETURN(cudaHostRegister(src_, bytes, 0));
+            numa_bind_node(src_id_);
+            src_ = aligned_alloc(page_size(), bytes);
+            OR_RETURN(cudaHostRegister(src_, bytes, cudaHostRegisterPortable | cudaHostRegisterMapped));
+            std::memset(src_, 0, bytes);
+        } else if (cudaMemcpyDeviceToHost == kind_) {
+            numa_bind_node(dst_id_);
+            dst_ = aligned_alloc(page_size(), bytes);
+            OR_RETURN(cudaHostRegister(dst_, bytes, cudaHostRegisterPortable | cudaHostRegisterMapped));
+            std::memset(dst_, 0, bytes);
         } else {
             assert(0 && "Unexpected kind_");
         }
+        numa_bind_node(-1);
+        return cudaSuccess;
     }
 
     cudaError_t cpu_free() {
         if (cudaMemcpyHostToDevice == kind_) {
-            OR_RETURN(cudaHostUnregiser(dst_));
-            free(dst_);
-        } else if { cudaMemcpyDeviceToHost == kind_} {
-            OR_RETURN(cudaHostUnregister(src_));
+            OR_RETURN(cudaHostUnregister(dst_));
             free(src_);
+        } else if (cudaMemcpyDeviceToHost == kind_) {
+            OR_RETURN(cudaHostUnregister(src_));
+            free(dst_);
         } else {
             assert(0 && "Unexpected kind_");
         }        
+        return cudaSuccess;
     }
-}
+};
+
+#undef OR_RETURN
+
