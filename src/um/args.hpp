50520f5061ba54e00a01ac46b364d513c2261847
Carl Pearson <pearson@illinois.edu>
2020-03-06 15:47:52 -0600

    remove unused variable

--- src/um/args.hpp
+++ src/um/args.hpp
@@ -3,8 +3,7 @@
 static void CustomArguments(benchmark::internal::Benchmark* b) {
   b->Args({0})->ArgName("log2(N)");
   for (int i = 12; i <= 33; ++i)
-    for (auto j : {4096})
-      b->Args({i})->ArgName("log2(N)");
+    b->Args({i})->ArgName("log2(N)");
 }
 
 #define BYTE_ARGS() Apply(CustomArguments)
