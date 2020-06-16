f2b5a4a616e34774b7426bb8c31d0ee9dfe4a4ec
Carl Pearson <carl.w.pearson@gmail.com>
2020-06-16 15:08:39 -0500

    [travis]

--- ci/env.sh
+++ ci/env.sh
@@ -4,7 +4,7 @@ CMAKE_PREFIX=$HOME/cmake
 if [ -z ${USE_CUDA+x} ]; then USE_CUDA=1; fi
 if [ -z ${USE_OPENMP+x} ]; then USE_OPENMP=1; fi
 if [ -z ${USE_NUMA+x} ]; then USE_NUMA=1; fi
-if [ -z ${USE_NVTX+x} ]; then USE_NUMA=1; fi
+if [ -z ${USE_NVTX+x} ]; then USE_NVTX=1; fi
 
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
