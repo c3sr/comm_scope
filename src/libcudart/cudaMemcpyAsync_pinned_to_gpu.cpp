5f923a9d5f8fdf86671742b7233eb92ec2bcd5eb
Carl Pearson <carl.w.pearson@gmail.com>
2020-06-17 16:58:02 -0500

    add cudaMemcpyAsync_GPUToPinned and cudaMemcpyAsync_PinnedToGPU

---
+++ src/libcudart/cudaMemcpyAsync_pinned_to_gpu.cpp
@@ -0,0 +1,73 @@
+/* Measure the runtime cost of cudaMemcpy3DPeerAsync
+ */
+
+#include "sysbench/sysbench.hpp"
+
+#define NAME "Comm_cudart_cudaMemcpyAsync_PinnedToGPU"
+
+auto Comm_cudart_cudaMemcpyAsync_PinnedToGPU = [](benchmark::State &state,
+                                            const int gpu, const int numa_id) {
+
+  numa::ScopedBind binder(numa_id);
+
+  OR_SKIP_AND_RETURN(cuda_reset_device(gpu), "failed to reset CUDA device");
+
+
+  // Create One stream per copy
+  cudaStream_t stream = nullptr;
+  OR_SKIP_AND_RETURN(cudaStreamCreate(&stream), "failed to create stream");
+
+  // fixed-size transfer
+  const size_t bytes = 1024 * 10;
+
+  void *src, *dst;
+
+  // allocate on host
+  src = aligned_alloc(page_size(), bytes);
+  std::memset(src, 0, bytes);
+  OR_SKIP_AND_RETURN(cudaHostRegister(src, bytes, cudaHostRegisterPortable), "failed to register allocation");
+
+  // allocate on gpu and enable peer access
+  OR_SKIP_AND_RETURN(cudaSetDevice(gpu), "failed to set device");
+  OR_SKIP_AND_RETURN(cudaMalloc(&dst, bytes), "failed to perform cudaMalloc3D");
+  OR_SKIP_AND_RETURN(cudaMemset(dst, 0, bytes),
+          "failed to perform src cudaMemset");
+
+  for (auto _ : state) {
+    // Start copy
+    cudaError_t err = cudaMemcpyAsync(dst, src, bytes, cudaMemcpyHostToDevice, stream);
+
+    // measure one copy at a time
+    state.PauseTiming();
+    OR_SKIP_AND_BREAK(err,
+                      "failed to start cudaMemcpyAsync");
+
+    OR_SKIP_AND_BREAK(cudaStreamSynchronize(stream),
+                      "failed to synchronize");
+    state.ResumeTiming();
+  }
+
+  state.counters["gpu"] = gpu;
+  state.counters["numa_id"] = numa_id;
+
+  OR_SKIP_AND_RETURN(cudaStreamDestroy(stream), "cudaStreamDestroy");
+  OR_SKIP_AND_RETURN(cudaHostUnregister(src), "");
+  free(src);
+  OR_SKIP_AND_RETURN(cudaFree(dst), "failed to cudaFree");
+};
+
+static void registerer() {
+  std::string name;
+  for (size_t i = 0; i < numa::ids().size(); ++i) {
+    for (size_t j = i; j < unique_cuda_device_ids().size(); ++j) {
+      auto numa_id = numa::ids()[i];
+      auto gpu = unique_cuda_device_ids()[j];
+name = std::string(NAME) + "/" + std::to_string(numa_id) + "/" +
+                 std::to_string(gpu);
+          benchmark::RegisterBenchmark(
+              name.c_str(), Comm_cudart_cudaMemcpyAsync_PinnedToGPU, gpu, numa_id)->UseRealTime();
+        }
+      }
+    }
+
+SYSBENCH_AFTER_INIT(registerer, NAME);
--- src/prefetch/gpu_to_host.cu
+++ src/prefetch/gpu_to_host.cu
@@ -1,31 +1,16 @@
 #if __CUDACC_VER_MAJOR__ >= 8
 
-#include <cassert>
-
-#include <cuda_runtime.h>
-#if USE_NUMA
-#include <numa.h>
-#include "init/numa.hpp"
-#endif // USE_NUMA
-
- #include "sysbench/sysbench.hpp"
- 
+#include "sysbench/sysbench.hpp"
 
 #include "args.hpp"
 
 #define NAME "Comm_UM_Prefetch_GPUToHost"
 
-auto Comm_UM_Prefetch_GPUToHost = [](benchmark::State &state,
-#if USE_NUMA
-const int numa_id,
-#endif // USE_NUMA
-const int cuda_id) {
+auto Comm_UM_Prefetch_GPUToHost = [](benchmark::State &state, const int numa_id,
+                                     const int cuda_id) {
+  const auto bytes = 1ULL << static_cast<size_t>(state.range(0));
 
-  const auto bytes  = 1ULL << static_cast<size_t>(state.range(0));
-
-#if USE_NUMA
   numa::bind_node(numa_id);
-#endif // USE_NUMA
 
   if (PRINT_IF_ERROR(cuda_reset_device(cuda_id))) {
     state.SkipWithError(NAME " failed to reset device");
@@ -90,34 +75,22 @@ const int cuda_id) {
   state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(bytes));
   state.counters["bytes"] = bytes;
   state.counters["cuda_id"] = cuda_id;
-#if USE_NUMA
   state.counters["numa_id"] = numa_id;
-#endif // USE_NUMA
 
-#if USE_NUMA
   // reset to run on any node
   numa::bind_node(-1);
-#endif // USE_NUMA
 };
 
 static void registerer() {
   for (auto cuda_id : unique_cuda_device_ids()) {
-#if USE_NUMA
     for (auto numa_id : numa::ids()) {
-#endif // USE_NUMA
-      std::string name = std::string(NAME)
-#if USE_NUMA 
-                       + "/" + std::to_string(numa_id) 
-#endif // USE_NUMA
-                       + "/" + std::to_string(cuda_id);
+      std::string name = std::string(NAME) + "/" + std::to_string(numa_id) +
+                         "/" + std::to_string(cuda_id);
       benchmark::RegisterBenchmark(name.c_str(), Comm_UM_Prefetch_GPUToHost,
-#if USE_NUMA
-        numa_id,
-#endif // USE_NUMA
-        cuda_id)->SMALL_ARGS()->UseManualTime();
-#if USE_NUMA
+                                   numa_id, cuda_id)
+          ->SMALL_ARGS()
+          ->UseManualTime();
     }
-#endif // USE_NUMA
   }
 }
 
