ebebfbc154b29bae01bf1ca7f50687f1d453ba97
Carl Pearson <carl.w.pearson@gmail.com>
2020-06-18 11:38:45 -0500

    remove USE_NUMA

--- src/libc/malloc.cpp
+++ src/libc/malloc.cpp
@@ -1,73 +1,42 @@
- #include "sysbench/sysbench.hpp"
- 
+#include "sysbench/sysbench.hpp"
 
 #include "args.hpp"
 
-
 #define NAME "Comm_UM_Malloc"
 
-#define OR_SKIP(stmt) \
-  if (PRINT_IF_ERROR(stmt)) { \
-    state.SkipWithError(NAME); \
-    return; \
-}
-
-auto Comm_UM_Malloc = [] (benchmark::State &state
-  #if USE_NUMA
-  , const int numa_id
-  #endif // USE_NUMA
-  ) {
-
+auto Comm_UM_Malloc = [](benchmark::State &state, const int numa_id) {
   const auto bytes = 1ULL << static_cast<size_t>(state.range(0));
 
-#if USE_NUMA
-  numa::bind_node(numa_id);
-#endif
+  numa::ScopedBind binder(numa_id);
 
   void *ptr = nullptr;
-  
+
   for (auto _ : state) {
     auto start = std::chrono::system_clock::now();
     benchmark::DoNotOptimize(ptr = malloc(bytes));
     auto stop = std::chrono::system_clock::now();
     if (!ptr) {
-      state.SkipWithError(NAME " failed to allocate");
+      state.SkipWithError("failed to allocate");
     }
     free(ptr);
-    double seconds = std::chrono::duration_cast<std::chrono::nanoseconds>(stop-start).count();
+    double seconds =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
+            .count();
     state.SetIterationTime(seconds / 1e9);
   }
 
   state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(bytes));
   state.counters["bytes"] = bytes;
-#if USE_NUMA
   state.counters["numa_id"] = numa_id;
-#endif // USE_NUMA
-
-#if USE_NUMA
-  numa::bind_node(-1);
-#endif
 };
 
 static void registerer() {
-#if USE_NUMA
-    for (auto numa_id : numa::ids()) {
-#endif // USE_NUMA
-      std::string name = std::string(NAME)
-#if USE_NUMA 
-                       + "/" + std::to_string(numa_id) 
-#endif // USE_NUMA
-      ;
-      benchmark::RegisterBenchmark(name.c_str(), Comm_UM_Malloc
-#if USE_NUMA
-        ,numa_id
-#endif // USE_NUMA
-        )->BYTE_ARGS()->UseManualTime();
-#if USE_NUMA
-    }
-#endif // USE_NUMA
+  for (auto numa_id : numa::ids()) {
+    std::string name = std::string(NAME) + "/" + std::to_string(numa_id);
+    benchmark::RegisterBenchmark(name.c_str(), Comm_UM_Malloc, numa_id)
+        ->BYTE_ARGS()
+        ->UseManualTime();
+  }
 }
 
 SYSBENCH_AFTER_INIT(registerer, NAME);
-
-
