4e59d661b2caf33948b3f58d8cdf1bdc1d72043d
Carl Pearson <carl.w.pearson@gmail.com>
2020-06-18 11:41:14 -0500

    improve malloc timing

--- src/libc/malloc.cpp
+++ src/libc/malloc.cpp
@@ -2,9 +2,9 @@
 
 #include "args.hpp"
 
-#define NAME "Comm_UM_Malloc"
+#define NAME "Comm_libc_malloc"
 
-auto Comm_UM_Malloc = [](benchmark::State &state, const int numa_id) {
+auto Comm_libc_malloc = [](benchmark::State &state, const int numa_id) {
   const auto bytes = 1ULL << static_cast<size_t>(state.range(0));
 
   numa::ScopedBind binder(numa_id);
@@ -12,17 +12,13 @@ auto Comm_UM_Malloc = [](benchmark::State &state, const int numa_id) {
   void *ptr = nullptr;
 
   for (auto _ : state) {
-    auto start = std::chrono::system_clock::now();
     benchmark::DoNotOptimize(ptr = malloc(bytes));
-    auto stop = std::chrono::system_clock::now();
+    state.PauseTiming();
     if (!ptr) {
       state.SkipWithError("failed to allocate");
     }
     free(ptr);
-    double seconds =
-        std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
-            .count();
-    state.SetIterationTime(seconds / 1e9);
+    state.ResumeTiming();
   }
 
   state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(bytes));
@@ -33,9 +29,8 @@ auto Comm_UM_Malloc = [](benchmark::State &state, const int numa_id) {
 static void registerer() {
   for (auto numa_id : numa::ids()) {
     std::string name = std::string(NAME) + "/" + std::to_string(numa_id);
-    benchmark::RegisterBenchmark(name.c_str(), Comm_UM_Malloc, numa_id)
-        ->BYTE_ARGS()
-        ->UseManualTime();
+    benchmark::RegisterBenchmark(name.c_str(), Comm_libc_malloc, numa_id)
+        ->BYTE_ARGS();
   }
 }
 
