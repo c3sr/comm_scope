7721c721568b11bed63a65cee74365344c74ba12
Carl Pearson <carl.w.pearson@gmail.com>
2020-07-22 07:10:40 -0600

    Add CUDA 11 build to travis
    commit 6f3939c2114a321ece4675809dc7ad27d3f1c58e
    Author: Carl Pearson <carl.w.pearson@gmail.com>
    Date:   Wed Jul 22 06:58:03 2020 -0600
    
        wget quieter
    
    commit 175afed5e6a66f438f0441bc3b89d6c65cb71743
    Author: Carl Pearson <carl.w.pearson@gmail.com>
    Date:   Wed Jul 22 06:54:23 2020 -0600
    
        CUDA 11 build

--- ci/install.sh
+++ ci/install.sh
@@ -34,13 +34,23 @@ if [[ $USE_CUDA != "0" ]]; then
 
     if [[ $TRAVIS_CPU_ARCH == "ppc64le" ]]; then
         CUDA102="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/ppc64el/cuda-repo-ubuntu1804_10.2.89-1_ppc64el.deb"
+        CUDA110="https://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda-repo-ubuntu1804-11-0-local_11.0.2-450.51.05-1_ppc64el.deb"
     elif [[ $TRAVIS_CPU_ARCH == "amd64" ]]; then
-        CUDA102="http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.2.89-1_amd64.deb"
+        CUDA102="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.2.89-1_amd64.deb"
+        CUDA110="https://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda-repo-ubuntu1804-11-0-local_11.0.2-450.51.05-1_amd64.deb"
     fi
 
-    wget -SL $CUDA102 -O cuda.deb
-    sudo dpkg -i cuda.deb
-    sudo apt-get update 
-    sudo apt-get install -y --no-install-recommends \
-    cuda-toolkit-10-2
+    if [[ $CUDA_VERSION == "102" ]]; then
+        wget -qSL $CUDA102 -O cuda.deb
+        sudo dpkg -i cuda.deb
+        sudo apt-get update 
+        sudo apt-get install -y --no-install-recommends \
+        cuda-toolkit-10-2
+    else 
+        wget -qSL $CUDA110 -O cuda.deb
+        sudo dpkg -i cuda.deb
+        sudo apt-get update 
+        sudo apt-get install -y --no-install-recommends \
+        cuda-toolkit-11-0
+    fi
 fi
