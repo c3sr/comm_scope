dcf425a509b5cf05aba415967d06a37112be5f7b
Carl Pearson <pearson@illinois.edu>
2022-12-15 11:48:25 -0500

    update crusher evironment notes

--- crusher.md
+++ crusher.md
@@ -1,10 +1,11 @@
 ## OLCF Crusher
 
 ```
-salloc -A csc465 -J interactive -t 03:00:00 -p batch -N 1 -n 1 -c 64
+salloc -A CSC465_crusher -J interactive -t 03:00:00 -p batch -N 1 -n 1 -c 64
 ```
 
 ```
+mkdir build-crusher && cd build-crusher
 source ../load-env.sh
 cmake .. \
 -DCMAKE_CXX_COMPILER=hipcc \
--- load-env.sh
+++ load-env.sh
@@ -6,7 +6,7 @@ echo_and_do() {
 if [[ ${LMOD_SYSTEM_NAME} =~ crusher ]]; then
     echo "LMOD_SYSTEM_NAME matched crusher"
     echo_and_do module load cmake
-    echo_and_do module load rocm/5.3.0
+    echo_and_do module load rocm/5.4.0
 elif [[ $(hostname) =~ caraway ]]; then
     echo "LMOD_SYSTEM_NAME matched caraway"
     echo_and_do module load cmake
