fc51bacc09ef03ae3642b033b0bc1cd0d1cd9b2c
Carl Pearson <pearson@illinois.edu>
2022-12-16 18:14:54 -0500

    update cudaMemcpyAsync_Host.sh

--- scripts/summit/cudaMemcpyAsync_Host.sh
+++ scripts/summit/cudaMemcpyAsync_Host.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
-#BSUB -W 1:00
+#BSUB -W 2:00
 #BSUB -nnodes 1
-#BSUB -P csc362
-#BSUB -J cudaMemcpyAsync_Host
-
-module reset
-module load gcc
-module load cuda
+#BSUB -P csc465
+#BSUB -J cudaMemcpyAsync_Host 
+#BSUB -o cudaMemcpyAsync_Host.o%J
+#BSUB -e cudaMemcpyAsync_Host.e%J
 
 set -eou pipefail
 
-export SCRATCH=/gpfs/alpine/scratch/cpearson/csc362
+export ROOT=$HOME/repos/comm_scope
+export SCRATCH=/gpfs/alpine/scratch/cpearson/csc465
+export SCRIPTS=$ROOT/scripts/summit
+export CSV=cudaMemcpyAsync_Host.csv
+
+. $ROOT/load-env.sh
 
 date
 # -n (total rs)
 # -g (gpus per rs)
 # -a (MPI tasks per rs)
 # -c (cores per rs)
-jsrun -n1 -a1 -g6 -c42 -b rs js_task_info ../../build/comm_scope \
+jsrun -n1 -a1 -g6 -c42 -b rs js_task_info $ROOT/build-summit/comm_scope \
 --benchmark_out_format=csv \
---benchmark_out=$SCRATCH/cudaMemcpyAsync_Host.csv \
---benchmark_filter="Comm_cudaMemcpyAsync_(HostToGPU|GPUToHost).*/0/(0|3)"
+--benchmark_out=$SCRATCH/$CSV \
+--benchmark_filter="cudaMemcpyAsync_(HostToGPU|GPUToHost).*/0/(0|3)"
+
+mv -v $SCRATCH/$CSV $SCRIPTS/$CSV
 
 date
