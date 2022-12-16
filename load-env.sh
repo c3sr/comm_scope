661b6c97b89cdaf8779d3ffa2480c4c7e7d5f28f
Carl Pearson <pearson@illinois.edu>
2022-12-16 17:41:43 -0500

    summit

--- load-env.sh
+++ load-env.sh
@@ -7,6 +7,10 @@ if [[ ${LMOD_SYSTEM_NAME} =~ crusher ]]; then
     echo "LMOD_SYSTEM_NAME matched crusher"
     echo_and_do module load cmake
     echo_and_do module load rocm/5.4.0
+elif [[ ${LMOD_SYSTEM_NAME} =~ summit ]]; then
+    echo "LMOD_SYSTEM_NAME matched summit"
+    echo_and_do module load cmake
+    echo_and_do module load cuda/11.5.2
 elif [[ ${HOSTNAME} =~ caraway ]]; then
     echo "LMOD_SYSTEM_NAME matched caraway"
     echo_and_do module load cmake
